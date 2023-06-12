7302c0cc5eaa26ddf344e1efb1343bed72a3b543
Xavier Cabello <100261001+juanxavier357@users.noreply.github.com>
2023-06-12 11:30:04 -0500

    Update/table hotels (#9)
    * fix: add folder with your files reservation
    
    * fix: update in the columns of the hotels table (hotel, checkIn, checkOut and guests)
    
    * fix: update in the columns of the hotels table (hotel, checkIn, checkOut and guests)
    
    ---------
    
    Co-authored-by: Michael <62442141+Mike2020x@users.noreply.github.com>

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,12 +1,14 @@
-import express, { Application } from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
+import express, { Application } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 function configExpress(app: Application) {
-    app.use(cors())
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(morgan('dev'))
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(morgan('dev'));
+
+  // Agrega cualquier otra configuración de Express que necesites
 }
 
-export default configExpress
+export default configExpress;
