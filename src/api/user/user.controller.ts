f5a168c0586c02efaf26ef68eb7eebd272cf868d
Michael <62442141+Mike2020x@users.noreply.github.com>
2023-06-12 17:46:01 -0500

    Feat/amenity (#13)
    * feat: :art: add improvement of schema
    
    * fix: add .github pull_request_template.md
    
    * feat: :sparkles: add users folder and files
    
    * feat: :sparkles: add amenity folder and files
    
    ---------
    
    Co-authored-by: juanxavier357 <cabellosalirrosas@gmail.com>
    Co-authored-by: Xavier Cabello <100261001+juanxavier357@users.noreply.github.com>

---
+++ src/api/user/user.controller.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from './user.service'
+
+export async function getAllUsersHandler(req: Request, res: Response) {
+  const users = await getAllUsers()
+
+  return res.json(users)
+}
+
+export async function getUserHandler(req: Request, res: Response) {
+  const { id } = req.params
+
+  const user = await getUserById(id)
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'user not found',
+    })
+  }
+
+  return res.status(200).json(user)
+}
+
+export async function createUserHandler(req: Request, res: Response) {
+  const data = req.body
+
+  if (!data.name || !data.email) {
+    return res.status(400).json({
+      error: "name or email or password is missing",
+    });
+  }
+
+  const user = await createUser(data)
+
+  return res.status(201).json(user)
+}
+
+export async function updateUserHandler(req: Request, res: Response) {
+  const { id } = req.params;
+  const data = req.body;
+  const user = await updateUser(id, data);
+  if (!user) {
+    return res.status(404).json({
+      message: "user not Found",
+    });
+  }
+  return res.status(202).json(user);
+}
+
+export async function deleteUserHandler(req: Request, res: Response) {
+  const { id } = req.params
+
+  const user = await getUserById(id)
+
+  if (!user) {
+    return res.status(404).json({
+      message: 'user not found',
+    })
+  }
+
+  await deleteUser(id)
+
+  return res.json({ message: 'user deleted' })
+}
