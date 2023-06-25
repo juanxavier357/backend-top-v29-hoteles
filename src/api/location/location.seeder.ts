409defe804a077fb83a7d1bc472fe0182870908b
juanxavier357 <cabellosalirrosas@gmail.com>
2023-06-25 17:19:39 -0500

    feat: :sparkles: add seeder location
    50 hotel locations

---
+++ src/api/location/location.seeder.ts
@@ -0,0 +1,284 @@
+export const locationSeeder = [
+  {
+    id: 'Colombia_Hotel_1',
+    city: 'Cartagena',
+    address:
+      'Cra. 3 #31-23, Centro, Cartagena de Indias, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_2',
+    city: 'Cartagena',
+    address:
+      'Calle Del Torno 39-29, Cra. 8 #39-36, San Diego, Cartagena de Indias, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_3',
+    city: 'Bogota',
+    address:
+      'Cl. 73 #8-60, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_4',
+    city: 'Cartagena',
+    address:
+      'Ciénaga de Cholón, Barú, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_5',
+    city: 'Cartagena',
+    address:
+      'la No. 36 44, Universidad de Cartagena, Centro, Cartagena de Indias, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_6',
+    city: 'Bogota',
+    address:
+      'Cra. 7 #69a-22, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_7',
+    city: 'Cartagena',
+    address:
+      'Centro histórico, Calle Santo Domingo, Cra. 3 #33-63, Centro, Cartagena de Indias, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_8',
+    city: 'Medellin',
+    address:
+      'Cl. 3 Sur #43a-64, El Poblado, Medellín, El Poblado, Medellín, Antioquia, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_9',
+    city: 'Bogota',
+    address:
+      'Cra. 13 #85-80, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_10',
+    city: 'Cartagena',
+    address:
+      'Cra. 1 #5-01, Cartagena de Indias, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_11',
+    city: 'Medellin',
+    address:
+      'Cra. 43A #7-50, El Poblado, Medellín, El Poblado, Medellín, Antioquia, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_12',
+    city: 'Bogota',
+    address: 'Cl. 113 #7 - 65, Bogotá, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_13',
+    city: 'Cartagena',
+    address:
+      'Centro Histórico, Calle de Vélez Danies, No. 4 – 39, Cartagena de Indias, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_14',
+    city: 'Bogota',
+    address:
+      'Carrera 11#93-77, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_15',
+    city: 'Cartagena',
+    address: 'Carrera 8 #39-114 Barrio, Cartagena de Indias, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_16',
+    city: 'Bogota',
+    address: 'Ak. 9 #115 - 30, Usaquén, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_17',
+    city: 'Cartagena',
+    address: '130008, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_18',
+    city: 'Barranquilla',
+    address:
+      'Cl. 80 #51B - 25, Nte. Centro Historico, Barranquilla, Atlántico, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_19',
+    city: 'Bogota',
+    address: 'Cl. 113 #7 - 65, Bogotá, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_20',
+    city: 'Magdalena',
+    address:
+      'Cra. 2 #11A - 98, El Rodadero, Santa Marta, Gaira, Santa Marta, Magdalena, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_21',
+    city: 'Bogota',
+    address: 'Cra. 15 #100-11, Usaquén, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_22',
+    city: 'Boyaca',
+    address: 'Cl. 13 #7 63, Villa de Leyva, Boyacá, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_23',
+    city: 'Bogota',
+    address:
+      'Cra. 7 #72-41, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_24',
+    city: 'Cartagena',
+    address:
+      'Cielo mar, Cartagena de Indias, La Boquilla, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_25',
+    city: 'Bogota',
+    address: 'Ac. 100 #19A 83, Bogotá, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_26',
+    city: 'Bogota',
+    address:
+      'Cl. 95 #9-97, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_27',
+    city: 'Cartagena',
+    address:
+      'Cra. 1 #812, Cartagena de Indias, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_28',
+    city: 'Cartagena',
+    address: '130008, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_29',
+    city: 'Tolima',
+    address: 'Cra. 1A Sur #45-50, Ibagué, Tolima, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_30',
+    city: 'Cartagena',
+    address:
+      'Calle del Cuartel (Cra. 5) No. 36-118, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_31',
+    city: 'Medellin',
+    address:
+      'Calle 16, Variante #28-51, Las Palmas, Medellín, Antioquia, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_32',
+    city: 'Santander',
+    address: 'Cl. 47 #n 28-83, Sotomayor, Bucaramanga, Santander, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_33',
+    city: 'Cali',
+    address:
+      'Cra. 100b #11A-99, Ciudad Jardín, Cali, Valle del Cauca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_34',
+    city: 'Bogota',
+    address: 'No. 22-99, Cra. 60, Teusaquillo, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_35',
+    city: 'Medellin',
+    address: 'Carrera 42 ,1S-74, Medellín, Antioquia, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_36',
+    city: 'Barranquilla',
+    address:
+      'Cl. 76 #No. 56 29, Nte. Centro Historico, Barranquilla, Atlántico, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_37',
+    city: 'Cartagena',
+    address:
+      'Avenida Las Velas No 1-60, El Laguito, Cartagena de Indias, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_38',
+    city: 'Barranquilla',
+    address:
+      'Cl. 80 #51B - 25, Nte. Centro Historico, Barranquilla, Atlántico, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_39',
+    city: 'Cartagena',
+    address:
+      'Avenida Almirante Brion, El Laguito, Cartagena de Indias, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_40',
+    city: 'Cali',
+    address: 'Av. 6 D 36N-18, Cali, Valle del Cauca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_41',
+    city: 'Bogota',
+    address:
+      'Cl. 61 #5-39, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_42',
+    city: 'Bogota',
+    address: 'Av. C. 127 #15a-10, Bogotá, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_43',
+    city: 'Bogota',
+    address:
+      'Av. Pepe Sierra) Calle 116, 110111 #18b-60, Usaquén, Bogotá, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_44',
+    city: 'Bogota',
+    address:
+      'Cra. 13 #85-46, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_45',
+    city: 'Bogota',
+    address:
+      'Cl. 95 #9-97, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_46',
+    city: 'Bogota',
+    address: 'Parque de la 93 y del la zona T, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_47',
+    city: 'Cartagena',
+    address:
+      'Cra. 1 #N° 6 - 154, Cartagena de Indias, Provincia de Cartagena, Bolívar, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_48',
+    city: 'Medellin',
+    address: 'Carrera 42 ,1S-74, Medellín, Antioquia, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_49',
+    city: 'Bogota',
+    address:
+      'Cl. 93 #11-19, Localidad de Chapinero, Bogotá, Cundinamarca, Colombia',
+  },
+  {
+    id: 'Colombia_Hotel_50',
+    city: 'Bogota',
+    address: 'Cl. 114 #6-02, Bogotá, Colombia',
+  },
+];
