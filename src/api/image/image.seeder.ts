19b9cb2e22e09e4e7b624ff00992fae19f44cc9c
juanxavier357 <cabellosalirrosas@gmail.com>
2023-06-25 18:21:14 -0500

    feat: :sparkles: add contactsInfo seeder
    Add 50 contacsInfo and improvement in the seeder

--- src/api/image/image.seeder.ts
+++ src/api/image/image.seeder.ts
@@ -1,202 +1,252 @@
 export const imageSeeder = [
   {
     id: 'Colombia_Hotel_1',
-    url: 'https://cdn.pixabay.com/photo/2019/07/30/14/12/woman-4373078_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2019/07/30/14/12/woman-4373078_640.jpg',
+    hotelId: 'Colombia_Hotel_1',
   },
   {
     id: 'Colombia_Hotel_2',
     url: 'https://cdn.pixabay.com/photo/2016/11/18/22/21/restaurant-1837150_640.jpg',
+    hotelId: 'Colombia_Hotel_2',
   },
   {
     id: 'Colombia_Hotel_3',
-    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/17/hotel-389256_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2014/07/10/17/17/hotel-389256_640.jpg',
+    hotelId: 'Colombia_Hotel_3',
   },
   {
     id: 'Colombia_Hotel_4',
     url: 'https://cdn.pixabay.com/photo/2016/12/11/18/10/apartment-1899964_640.jpg',
+    hotelId: 'Colombia_Hotel_4',
   },
   {
     id: 'Colombia_Hotel_5',
     url: 'https://cdn.pixabay.com/photo/2017/05/31/10/23/manor-house-2359884_640.jpg',
+    hotelId: 'Colombia_Hotel_5',
   },
   {
     id: 'Colombia_Hotel_6',
-    url: 'https://cdn.pixabay.com/photo/2017/01/28/19/31/landscape-2016308_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2017/01/28/19/31/landscape-2016308_640.jpg',
+    hotelId: 'Colombia_Hotel_6',
   },
   {
     id: 'Colombia_Hotel_7',
     url: 'https://cdn.pixabay.com/photo/2013/07/19/00/18/water-165219_640.jpg',
+    hotelId: 'Colombia_Hotel_7',
   },
   {
     id: 'Colombia_Hotel_8',
-    url: 'https://cdn.pixabay.com/photo/2017/03/09/06/30/pool-2128578_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2017/03/09/06/30/pool-2128578_640.jpg',
+    hotelId: 'Colombia_Hotel_8',
   },
   {
     id: 'Colombia_Hotel_9',
     url: 'https://cdn.pixabay.com/photo/2014/05/18/19/15/walkway-347319_640.jpg',
+    hotelId: 'Colombia_Hotel_9',
   },
  {
     id: 'Colombia_Hotel_10',
-    url: 'https://cdn.pixabay.com/photo/2015/09/21/09/53/villa-cortine-palace-949547_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/09/21/09/53/villa-cortine-palace-949547_640.jpg',
+    hotelId: 'Colombia_Hotel_10',
   },
   {
     id: 'Colombia_Hotel_11',
     url: 'https://cdn.pixabay.com/photo/2014/05/17/18/03/lobby-346426_640.jpg',
+    hotelId: 'Colombia_Hotel_11',
   },
   {
     id: 'Colombia_Hotel_12',
-    url: 'https://cdn.pixabay.com/photo/2015/01/10/11/39/hotel-595121_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/01/10/11/39/hotel-595121_640.jpg',
+    hotelId: 'Colombia_Hotel_12',
   },
   {
     id: 'Colombia_Hotel_13',
     url: 'https://cdn.pixabay.com/photo/2018/08/06/19/49/design-3588214_640.jpg',
+    hotelId: 'Colombia_Hotel_13',
   },
  {
     id: 'Colombia_Hotel_14',
-    url: 'https://cdn.pixabay.com/photo/2017/06/11/12/33/swimming-2392283_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2017/06/11/12/33/swimming-2392283_640.jpg',
+    hotelId: 'Colombia_Hotel_14',
   },
   {
     id: 'Colombia_Hotel_15',
     url: 'https://cdn.pixabay.com/photo/2016/11/17/09/28/hotel-1831072_640.jpg',
+    hotelId: 'Colombia_Hotel_15',
   },
   {
     id: 'Colombia_Hotel_16',
-    url: 'https://cdn.pixabay.com/photo/2018/09/04/02/13/pool-bar-3652849_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2018/09/04/02/13/pool-bar-3652849_640.jpg',
+    hotelId: 'Colombia_Hotel_16',
   },
   {
     id: 'Colombia_Hotel_17',
     url: 'https://cdn.pixabay.com/photo/2019/06/13/17/08/round-window-4272049_640.jpg',
+    hotelId: 'Colombia_Hotel_17',
   },
  {
     id: 'Colombia_Hotel_18',
-    url: 'https://cdn.pixabay.com/photo/2015/09/15/11/04/hotel-940730_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/09/15/11/04/hotel-940730_640.jpg',
+    hotelId: 'Colombia_Hotel_18',
   },
   {
     id: 'Colombia_Hotel_19',
     url: 'https://cdn.pixabay.com/photo/2016/08/26/20/30/hotel-1623064_640.jpg',
+    hotelId: 'Colombia_Hotel_19',
   },
   {
     id: 'Colombia_Hotel_20',
-    url: 'https://cdn.pixabay.com/photo/2017/06/25/08/07/sunbeds-2439951_1280.jpg'
+    url: 'https://cdn.pixabay.com/photo/2017/06/25/08/07/sunbeds-2439951_1280.jpg',
+    hotelId: 'Colombia_Hotel_20',
   },
   {
     id: 'Colombia_Hotel_21',
     url: 'https://cdn.pixabay.com/photo/2016/07/08/23/36/beach-house-1505461_640.jpg',
+    hotelId: 'Colombia_Hotel_21',
   },
  {
     id: 'Colombia_Hotel_22',
-    url: 'https://cdn.pixabay.com/photo/2016/04/15/11/46/bedroom-1330846_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2016/04/15/11/46/bedroom-1330846_640.jpg',
+    hotelId: 'Colombia_Hotel_22',
   },
   {
     id: 'Colombia_Hotel_23',
     url: 'https://cdn.pixabay.com/photo/2020/02/01/06/12/living-room-4809587_640.jpg',
+    hotelId: 'Colombia_Hotel_23',
   },
   {
     id: 'Colombia_Hotel_24',
-    url: 'https://cdn.pixabay.com/photo/2021/11/08/00/30/living-room-6778197_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2021/11/08/00/30/living-room-6778197_640.jpg',
+    hotelId: 'Colombia_Hotel_24',
   },
   {
     id: 'Colombia_Hotel_25',
     url: 'https://cdn.pixabay.com/photo/2017/06/09/09/30/swimming-pool-2386261_640.jpg',
+    hotelId: 'Colombia_Hotel_25',
   },
  {
     id: 'Colombia_Hotel_26',
-    url: 'https://cdn.pixabay.com/photo/2016/08/29/20/52/los-angeles-1629161_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2016/08/29/20/52/los-angeles-1629161_640.jpg',
+    hotelId: 'Colombia_Hotel_26',
   },
   {
     id: 'Colombia_Hotel_27',
     url: 'https://cdn.pixabay.com/photo/2016/11/18/17/41/summer-1836046_640.jpg',
+    hotelId: 'Colombia_Hotel_27',
   },
   {
     id: 'Colombia_Hotel_28',
-    url: 'https://cdn.pixabay.com/photo/2016/05/03/14/05/bahrain-1369259_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2016/05/03/14/05/bahrain-1369259_640.jpg',
+    hotelId: 'Colombia_Hotel_28',
   },
   {
     id: 'Colombia_Hotel_29',
     url: 'https://cdn.pixabay.com/photo/2017/06/13/22/41/hotel-2400364_640.jpg',
+    hotelId: 'Colombia_Hotel_29',
   },
   {
     id: 'Colombia_Hotel_30',
-    url: 'https://cdn.pixabay.com/photo/2016/01/18/09/21/conference-room-1146244_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2016/01/18/09/21/conference-room-1146244_640.jpg',
+    hotelId: 'Colombia_Hotel_30',
   },
   {
     id: 'Colombia_Hotel_31',
     url: 'https://cdn.pixabay.com/photo/2015/04/20/17/38/swimming-pool-731886_640.jpg',
+    hotelId: 'Colombia_Hotel_31',
   },
   {
     id: 'Colombia_Hotel_32',
-    url: 'https://cdn.pixabay.com/photo/2017/04/28/22/14/room-2269591_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2017/04/28/22/14/room-2269591_640.jpg',
+    hotelId: 'Colombia_Hotel_32',
   },
   {
     id: 'Colombia_Hotel_33',
     url: 'https://cdn.pixabay.com/photo/2015/09/21/09/54/villa-cortine-palace-949552_640.jpg',
+    hotelId: 'Colombia_Hotel_33',
   },
 {
     id: 'Colombia_Hotel_34',
-    url: 'https://cdn.pixabay.com/photo/2015/09/22/13/16/hotel-951598_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/09/22/13/16/hotel-951598_640.jpg',
+    hotelId: 'Colombia_Hotel_34',
   },
   {
     id: 'Colombia_Hotel_35',
     url: 'https://cdn.pixabay.com/photo/2021/10/06/15/05/bedroom-6686058_640.jpg',
+    hotelId: 'Colombia_Hotel_35',
   },
   {
     id: 'Colombia_Hotel_36',
-    url: 'https://cdn.pixabay.com/photo/2014/08/17/23/45/hotel-420260_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2014/08/17/23/45/hotel-420260_640.jpg',
+    hotelId: 'Colombia_Hotel_36',
   },
   {
     id: 'Colombia_Hotel_37',
     url: 'https://cdn.pixabay.com/photo/2018/10/01/00/51/luxury-hotel-3715115_640.jpg',
+    hotelId: 'Colombia_Hotel_37',
   },
 {
     id: 'Colombia_Hotel_38',
-    url: 'https://cdn.pixabay.com/photo/2015/11/02/06/46/hotel-1018039_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/11/02/06/46/hotel-1018039_640.jpg',
+    hotelId: 'Colombia_Hotel_38',
   },
   {
     id: 'Colombia_Hotel_39',
     url: 'https://cdn.pixabay.com/photo/2019/02/22/21/56/hotels-4014509_640.jpg',
+    hotelId: 'Colombia_Hotel_39',
   },
   {
     id: 'Colombia_Hotel_40',
-    url: 'https://cdn.pixabay.com/photo/2014/05/30/18/30/vancouver-358515_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2014/05/30/18/30/vancouver-358515_640.jpg',
+    hotelId: 'Colombia_Hotel_40',
   },
   {
     id: 'Colombia_Hotel_41',
     url: 'https://cdn.pixabay.com/photo/2016/07/08/23/41/window-view-1505463_640.jpg',
+    hotelId: 'Colombia_Hotel_41',
   },
 {
     id: 'Colombia_Hotel_42',
-    url: 'https://cdn.pixabay.com/photo/2015/09/23/09/51/peace-953285_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/09/23/09/51/peace-953285_640.jpg',
+    hotelId: 'Colombia_Hotel_42',
   },
   {
     id: 'Colombia_Hotel_43',
     url: 'https://cdn.pixabay.com/photo/2017/07/11/22/16/resort-2495223_640.jpg',
+    hotelId: 'Colombia_Hotel_43',
   },
   {
     id: 'Colombia_Hotel_44',
-    url: 'https://cdn.pixabay.com/photo/2018/02/22/08/05/palma-3172367_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2018/02/22/08/05/palma-3172367_640.jpg',
+    hotelId: 'Colombia_Hotel_44',
   },
   {
     id: 'Colombia_Hotel_45',
     url: 'https://cdn.pixabay.com/photo/2014/04/05/11/19/hotel-315122_640.jpg',
+    hotelId: 'Colombia_Hotel_45',
   },
 {
     id: 'Colombia_Hotel_46',
-    url: 'https://cdn.pixabay.com/photo/2017/10/07/09/58/dominican-republic-2825981_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2017/10/07/09/58/dominican-republic-2825981_640.jpg',
+    hotelId: 'Colombia_Hotel_46',
   },
   {
     id: 'Colombia_Hotel_47',
     url: 'https://cdn.pixabay.com/photo/2014/01/22/06/26/swimming-pool-249624_640.jpg',
+    hotelId: 'Colombia_Hotel_47',
   },
   {
     id: 'Colombia_Hotel_48',
-    url: 'https://cdn.pixabay.com/photo/2015/09/16/21/17/sea-943428_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/09/16/21/17/sea-943428_640.jpg',
+    hotelId: 'Colombia_Hotel_48',
   },
   {
     id: 'Colombia_Hotel_49',
     url: 'https://cdn.pixabay.com/photo/2016/08/20/05/49/venice-italy-gritti-palace-1606928_640.jpg',
+    hotelId: 'Colombia_Hotel_49',
   },
 {
     id: 'Colombia_Hotel_50',
-    url: 'https://cdn.pixabay.com/photo/2015/09/19/10/00/villa-cortine-palace-947035_640.jpg'
+    url: 'https://cdn.pixabay.com/photo/2015/09/19/10/00/villa-cortine-palace-947035_640.jpg',
+    hotelId: 'Colombia_Hotel_50',
   },
 ]
