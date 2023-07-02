3aa4ef646a0db9de1191407b9a8f54807a5bd0f8
Mike2020x <62442141+Mike2020x@users.noreply.github.com>
2023-07-02 12:48:26 -0500

    ci: :sparkles: add ci config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended','standard-with-typescript'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     overrides: [
     ],
     parserOptions: {
